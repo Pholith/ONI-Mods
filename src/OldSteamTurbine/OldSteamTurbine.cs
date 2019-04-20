5bea70ffe18111f8797e116865329a522d76fbbf
Darkmauros <vincent22062@orange.fr>
2019-04-20 17:17:40 +0200

    OldSteamTurbine

---
+++ src/OldSteamTurbine/OldSteamTurbine.cs
@@ -0,0 +1,13 @@
+﻿using Harmony;
+namespace OldSteamTurbine
+{
+
+    [HarmonyPatch(typeof(SteamTurbineConfig), "CreateBuildingDef")]
+    public class OldSteamTurbine
+    {
+        public static void Postfix(SteamTurbineConfig __instance, ref BuildingDef __result)
+        {
+            __result.Deprecated = false;
+        }
+    }
+}
---
+++ src/OldSteamTurbine/Preview/Preview.md
@@ -0,0 +1,8 @@
+![image](screen.png)
+
+# Always 3 Interests
+
+## About
+You can now build the Old Steam Turbine.
+
+WARNING: The Old Steam turbine works, but some information about her is no longer available.
