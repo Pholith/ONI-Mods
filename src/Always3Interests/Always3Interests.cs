fc0eca1821e540b3c644cd51b07c0c508d1e3d9a
Darkmauros <vincent22062@orange.fr>
2019-04-20 11:17:11 +0200

    remove useless code

--- src/Always3Interests/Always3Interests.cs
+++ src/Always3Interests/Always3Interests.cs
@@ -8,41 +8,6 @@ using UnityEngine;
 
 namespace Always3Interests
 {
-    [HarmonyPatch(typeof(AirFilterConfig), "CreateBuildingDef")]
-    public class Class1
-    {
-        public static void Postfix(AirFilterConfig __instance, ref BuildingDef __result)
-        {
-            __result.PermittedRotations = PermittedRotations.FlipV;
-        }
-    }
-    [HarmonyPatch(typeof(SteamTurbineConfig2), "CreateBuildingDef")]
-    public class Class2
-    {
-        public static void Postfix(SteamTurbineConfig2 __instance, ref BuildingDef __result)
-        {
-            __result.PermittedRotations = PermittedRotations.R360;
-        }
-    }
-    /*[HarmonyPatch(typeof(MinionStartingStats), "GenerateTraits")]
-    public class Class3
-    {
-        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-        {
-            var codes = new List<CodeInstruction>(instructions);
-            for (int i = 0; i < codes.Count; i++)
-            {
-                if (codes[i].opcode == OpCodes.Ldc_I4_0)//98
-                {
-                    codes[i].opcode = OpCodes.Ldc_I4;
-                    codes[i].operand = -20;
-
-                }
-            }
-
-            return codes.AsEnumerable();
-        }
-    }*/
     [HarmonyPatch(typeof(MinionStartingStats), "GenerateAptitudes")]
     public class Class3
     {
@@ -59,10 +24,10 @@ namespace Always3Interests
                 if (i > 3) break;
 
             }
-            for (int i = 0; i < codes.Count; i++)
+            /*for (int i = 0; i < codes.Count; i++)
             {
                 Debug.Log("here " + codes[i].ToString());
-            }
+            }*/
 
             return codes.AsEnumerable();
         }
