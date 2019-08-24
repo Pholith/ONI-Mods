f4d21120d2bc88eeed03d9d1e4ceddd040cf0e14
Pholith <vincent22062@orange.fr>
2019-08-24 09:33:20 +0200

    achievement disabling patch

---
+++ src/AchievementReEnabler/AchievementPatch.cs
@@ -0,0 +1,43 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+
+namespace AchievementReEnabler
+{
+
+    // modify the save File to toggle debugWasUsed to false
+    [HarmonyPatch(typeof(Game))]
+    [HarmonyPatch("Load")]
+    class AchievementPatch
+    {
+
+        public static void Postfix(Game __instance)
+        {
+            __instance.debugWasUsed = false;
+        }
+    }
+
+
+    // Steam Unlock achievment that were unlocked when debugWasUsed was true
+    [HarmonyPatch(typeof(ColonyAchievementTracker))]
+    [HarmonyPatch("OnSpawn")]
+    class AchievementsReactiver
+    {
+
+        public static void Postfix(ColonyAchievementTracker __instance)
+        {
+            foreach (KeyValuePair<string, ColonyAchievementStatus> keyValuePair in __instance.achievements)
+            {
+                if (keyValuePair.Value.success && !keyValuePair.Value.failed)
+                {
+
+                    Type[] parameters_type = new Type[] { typeof(string) };
+                    object[] paramaters = new object[] { keyValuePair.Key };
+
+                    Traverse.Create<ColonyAchievementTracker>().Method("UnlockPlatformAchievement", parameters_type).GetValue(paramaters);
+                }
+            }
+
+        }
+    }
+}
