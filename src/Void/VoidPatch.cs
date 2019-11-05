cb3c43dd9c8eabe0b23db283f2958a7662ae95bc
Pholith <vincent22062@orange.fr>
2019-11-05 22:23:41 +0100

    void

---
+++ src/Void/VoidPatch.cs
@@ -0,0 +1,25 @@
+﻿using Harmony;
+using System.Reflection;
+using UnityEngine;
+
+namespace VoidPatch
+{
+    [HarmonyPatch(typeof(Db), "Initialize")]
+    class VoidPatch
+    {
+
+        public static LocString NAME = "Void";
+        public static LocString DESCRIPTION = "Description of void. </smallcaps>\n\n";
+
+        public static void Prefix()
+        {
+            // Add strings used in Fuleria.yaml
+            Strings.Add($"STRINGS.WORLDS.VOID.NAME", NAME);
+            Strings.Add($"STRINGS.WORLDS.VOID.DESCRIPTION", DESCRIPTION);
+
+            // Generate a translation .pot 
+            ModUtil.RegisterForTranslation(typeof(VoidPatch));
+
+        }
+    }
+}
