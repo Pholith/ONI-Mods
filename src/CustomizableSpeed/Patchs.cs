a8045bd220c1c049f21510c3d35c065809ea21d9
Pholith <vincent22062@orange.fr>
2020-02-18 10:31:53 +0100

    started customizable speed mod

---
+++ src/CustomizableSpeed/Patchs.cs
@@ -0,0 +1,39 @@
+﻿using Harmony;
+using System;
+using UnityEngine;
+
+namespace CustomizableSpeed
+{
+	[HarmonyPatch(typeof(SpeedControlScreen), "OnChanged")]
+	
+	internal static class CustomizableSpeedPatch
+	{
+		private static bool Prefix(SpeedControlScreen __instance)
+		{
+			bool isPaused = __instance.IsPaused;
+			if (isPaused)
+			{
+				Time.timeScale = 0f;
+			}
+			else
+			{
+				switch (__instance.GetSpeed())
+				{
+					case 0:
+						Time.timeScale = 5f;
+						break;
+					case 1:
+						Time.timeScale = 5f;
+						break;
+					case 2:
+						Time.timeScale = 5f;
+						break;
+
+					default:
+						break;
+				}
+			}
+			return false;
+		}
+	}
+}
