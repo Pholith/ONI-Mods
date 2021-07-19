6260f2ee6d997edf571d177b4784a1251cc20436
Pholith <vincent22062@gmail.com>
2021-07-19 19:05:54 +0200

    working on EmptyWorlds

--- src/EmptyWorlds/Patches.cs
+++ src/EmptyWorlds/Patches.cs
@@ -3,6 +3,10 @@ using KMod;
 using PeterHan.PLib.Core;
 using PeterHan.PLib.Database;
 using PeterHan.PLib.Options;
+using ProcGenGame;
+using STRINGS;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace EmptyWorlds
 {
@@ -31,16 +35,49 @@ namespace EmptyWorlds
     [HarmonyPatch(typeof(TemplateCache), "GetTemplate")]
     public class TemplateCache_GetTemplate
     {
-        /*public static void Postfix(ref TemplateContainer __result)
+        public static void Postfix(ref TemplateContainer __result)
         {
             if (EmptyWorlds.Settings.RemoveBunkerAtWorldgen)
             {
                 if (__result.name == "poi_bunker_skyblock")
                 {
-                    //__result = new TemplateContainer();
+                    __result = new TemplateContainer();
+                    var list = new List<TemplateClasses.Cell>();
+                    list.Add(new TemplateClasses.Cell(0, 0, SimHashes.Vacuum, 1, 0, "", 0));
+                    __result.Init(list, new List<TemplateClasses.Prefab>(), new List<TemplateClasses.Prefab>(), new List<TemplateClasses.Prefab>(), new List<TemplateClasses.Prefab>());
                 }
             }
-        }*/
+        }
     }
 
+    [HarmonyPatch(typeof(WorldGen), "DrawWorldBorder")]
+    public class WorldGen_DrawWorldBorder
+    {
+        private static void Prefix(WorldGen __instance, Sim.Cell[] cells, Chunk world, SeededRandom rnd, ref HashSet<int> borderCells, ref List<RectInt> poiBounds, WorldGen.OfflineCallbackFunction updateProgressFn)
+        {
+            bool boolSetting = __instance.Settings.GetBoolSetting("DrawWorldBorderBottom");
+            int intSetting = __instance.Settings.GetIntSetting("WorldBorderThickness");
+            int intSetting2 = __instance.Settings.GetIntSetting("WorldBorderRange");
+
+            byte new_elem_idx = (byte)ElementLoader.elements.IndexOf(WorldGen.unobtaniumElement);
+            float temperature = WorldGen.unobtaniumElement.defaultValues.temperature;
+            float mass = WorldGen.unobtaniumElement.defaultValues.mass;
+
+            for (int l = 0; l < world.size.x; l++)
+            {
+                updateProgressFn(UI.WORLDGEN.DRAWWORLDBORDER.key, l / world.size.x * 0.66f + 0.33f, WorldGenProgressStages.Stages.DrawWorldBorder);
+                var num9 = Mathf.Max(-intSetting2, Mathf.Min(rnd.RandomRange(-2, 2), intSetting2));
+                for (int m = 0; m < intSetting + num9; m++)
+                {
+                    int num13 = Grid.XYToCell(l, m);
+                    if (boolSetting)
+                    {
+                        borderCells.Add(num13);
+                        cells[num13].SetValues(new_elem_idx, temperature, mass);
+                    }
+                }
+
+            }
+        }
+    }
 }
