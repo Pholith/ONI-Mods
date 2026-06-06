21589457b5a67e287e4db29d0fe5593b2b10c336
Pholith <victoire22062@gmail.com>
2026-06-07 00:05:02 +0200

    Trying to make myself as a dup

---
+++ src/PholithDuplicant/PholithDuplicant.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+using KMod;
+using PeterHan.PLib.Options;
+using Pholib;
+
+namespace PholithDuplicant
+{
+    public class PholithDuplicant : UserMod2
+    {
+
+        public static PholithOptions Settings;
+
+        public override void OnLoad(Harmony harmony)
+        {
+            base.OnLoad(harmony);
+            new POptions().RegisterOptions(this, typeof(PholithOptions));
+
+            // Init PLib and settings
+            ///PUtil.InitLibrary();
+
+            Settings = POptions.ReadSettings<PholithOptions>();
+            if (Settings == null)
+            {
+                Settings = new PholithOptions();
+            }
+        }
+
+        public static void ReadSettings()
+        {
+            Logs.Log("Loading settings");
+            Settings = POptions.ReadSettings<PholithOptions>();
+            if (Settings == null)
+            {
+                Settings = new PholithOptions();
+            }
+        }
+    }
+    /*
+    [HarmonyPatch("PersonalityManager", "ReadPersonalities")]
+    public class Dupery_ReadPersonalities_Patch
+    {
+        public static void Postfix(Dictionary<string, PersonalityOutline> __result)
+        {
+
+        }
+    }*/
+}
