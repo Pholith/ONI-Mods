0575bd13ead80b2858a86395ba8e967853e9a3e8
Pholith <victoire22062@gmail.com>
2026-07-28 14:52:40 +0200

    Updated Customizable Speed and added a tooltip

--- src/CustomizableSpeed/Patchs.cs
+++ src/CustomizableSpeed/Patchs.cs
@@ -2,6 +2,7 @@
 using KMod;
 using PeterHan.PLib.Core;
 using PeterHan.PLib.Options;
+using STRINGS;
 using UnityEngine;
 
 namespace CustomizableSpeed
@@ -37,11 +38,38 @@ namespace CustomizableSpeed
         }
     }
 
+    // Show the speed on hover
+    [HarmonyPatch(typeof(SpeedControlScreen), nameof(SpeedControlScreen.ResetToolTip))]
+    public class SpeedControlScreen_ResetToolTipPatch
+    {
+        public static void Postfix(SpeedControlScreen __instance, TextStyleSetting ___TooltipTextStyle)
+        {
+            if (GameOnLoadPatch.Settings == null)
+            {
+                GameOnLoadPatch.ReadSettings();
+            }
+            __instance.speedButtonWidget_slow.GetComponent<ToolTip>().ClearMultiStringTooltip();
+            __instance.speedButtonWidget_medium.GetComponent<ToolTip>().ClearMultiStringTooltip();
+            __instance.speedButtonWidget_fast.GetComponent<ToolTip>().ClearMultiStringTooltip();
+            __instance.speedButtonWidget_slow.GetComponent<ToolTip>().AddMultiStringTooltip($"{GameUtil.ReplaceHotkeyString(UI.TOOLTIPS.SPEEDBUTTON_SLOW, Action.CycleSpeed)} (x{GameOnLoadPatch.Settings.slowSpeed})", ___TooltipTextStyle);
+            __instance.speedButtonWidget_medium.GetComponent<ToolTip>().AddMultiStringTooltip($"{GameUtil.ReplaceHotkeyString(UI.TOOLTIPS.SPEEDBUTTON_SLOW, Action.CycleSpeed)} (x{GameOnLoadPatch.Settings.normalSpeed})", ___TooltipTextStyle);
+            __instance.speedButtonWidget_fast.GetComponent<ToolTip>().AddMultiStringTooltip($"{GameUtil.ReplaceHotkeyString(UI.TOOLTIPS.SPEEDBUTTON_SLOW, Action.CycleSpeed)} (x{GameOnLoadPatch.Settings.superSpeed})", ___TooltipTextStyle);
+        }
+    }
+    // Show the speed on hover
+    [HarmonyPatch(typeof(SpeedControlScreen), "OnPrefabInit")]
+    public class SpeedControlScreen_OnPrefabInitPatch
+    {
+        public static void Postfix(SpeedControlScreen __instance)
+        {
+            __instance.ResetToolTip();
+        }
+    }
 
     [HarmonyPatch(typeof(SpeedControlScreen), "OnChanged")]
     public static class SpeedControlPatchOnChanged
     {
-        public static bool Prefix(SpeedControlScreen __instance)
+        public static void Postfix(SpeedControlScreen __instance)
         {
             if (GameOnLoadPatch.Settings == null)
             {
@@ -51,26 +79,24 @@ namespace CustomizableSpeed
             if (__instance.IsPaused)
             {
                 Time.timeScale = 0f;
+                return;
             }
-            else
+            switch (__instance.GetSpeed())
             {
-                switch (__instance.GetSpeed())
-                {
-                    case 0:
-                        Time.timeScale = GameOnLoadPatch.Settings.slowSpeed;
-                        break;
-                    case 1:
-                        Time.timeScale = GameOnLoadPatch.Settings.normalSpeed;
-                        break;
-                    case 2:
-                        Time.timeScale = GameOnLoadPatch.Settings.superSpeed;
-                        break;
+                case 0:
+                    Time.timeScale = GameOnLoadPatch.Settings.slowSpeed;
+                    break;
+                case 1:
+                    Time.timeScale = GameOnLoadPatch.Settings.normalSpeed;
+                    break;
+                case 2:
+                    Time.timeScale = GameOnLoadPatch.Settings.superSpeed;
+                    break;
 
-                    default:
-                        break;
-                }
+                default:
+                    break;
             }
-            return false;
+            return;
         }
     }
 }
